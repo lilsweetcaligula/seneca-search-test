81b0e266c59ebbd8dd675ba5c44f6a3deb8b505a
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2021-08-21 21:19:33 +0300

    Add more tests

--- search-test.js
+++ search-test.js
@@ -20,6 +20,145 @@ function supports_add(args = {}) {
 }
 
 
+function add(args = {}) {
+  const { seneca } = args
+  Assert(null != seneca, 'The "seneca" argument is required')
+  Assert(seneca.isSeneca, 'The "seneca" argument must be a Seneca instance')
+
+
+  describe('addition of documents to the search pool', () => {
+    /*
+     * NOTE: We cannot reliably tell whether the addition API works or not,
+     * until we test the searching API. Thus, our best bet is to ensure, that
+     * the addition API completes without errors under normal circumstances.
+     */
+    it('completes without errors', done => {
+      seneca.test(done)
+
+      const new_doc = {
+        id: 'aaaa',
+        lorem: 'lorem',
+        ipsum: 'ipsum'
+      }
+
+      seneca.act('sys:search,cmd:add', { doc: new_doc }, function (err, out) {
+        if (err) {
+          return done(err)
+        }
+
+        expect(out).toEqual(jasmine.objectContaining({ ok: true }))
+
+
+        seneca.act('sys:search,cmd:remove', { id: 'aaaa' }, function (err, out) {
+          if (err) {
+            return done(err)
+          }
+
+          Assert(out && out.ok)
+
+          return done()
+        })
+      })
+    })
+  })
+}
+
+
+function remove(args = {}) {
+  const { seneca } = args
+  Assert(null != seneca, 'The "seneca" argument is required')
+  Assert(seneca.isSeneca, 'The "seneca" argument must be a Seneca instance')
+
+
+  describe('deletion of documents to the search pool', () => {
+    /*
+     * NOTE: We cannot reliably tell whether the deletion API works or not,
+     * until we test the searching API. Thus, our best bet is to ensure, that
+     * the deletion API completes without errors under normal circumstances.
+     */
+    it('completes without errors', done => {
+      seneca.test(done)
+
+      const new_doc = {
+        id: 'aaaa',
+        lorem: 'lorem',
+        ipsum: 'ipsum'
+      }
+
+      seneca.act('sys:search,cmd:add', { doc: new_doc }, function (err, out) {
+        if (err) {
+          return done(err)
+        }
+
+        Assert(out && out.ok)
+
+        seneca.act('sys:search,cmd:remove', { id: 'aaaa' }, function (err, out) {
+          if (err) {
+            return done(err)
+          }
+
+          expect(out).toEqual(jasmine.objectContaining({ ok: true }))
+
+          return done()
+        })
+      })
+    })
+  })
+}
+
+
+function search(args = {}) {
+  const { seneca } = args
+  Assert(null != seneca, 'The "seneca" argument is required')
+  Assert(seneca.isSeneca, 'The "seneca" argument must be a Seneca instance')
+
+
+  describe('searching for a value', () => {
+    it('finds a document with a field equal to the value', done => {
+      seneca.test(done)
+
+      const doc = {
+        id: 'aaaa',
+        lorem: 'lorem',
+        ipsum: 'ipsum'
+      }
+
+      seneca.act('sys:search,cmd:add', { doc }, function (err, out) {
+        if (err) {
+          return done(err)
+        }
+
+        Assert(out && out.ok)
+
+
+        const search_args = { query: 'lorem' }
+
+        seneca.act('sys:search,cmd:search', search_args, function (err, search) {
+          if (err) {
+            return done(err)
+          }
+
+          seneca.act('sys:search,cmd:remove', { id: 'aaaa' }, function (err, del) {
+            expect(search).toEqual(jasmine.objectContaining({
+              ok: true,
+              data: {
+                hits: [
+                  { id: 'aaaa', doc }
+                ]
+              }
+            }))
+
+            Assert(del && del.ok)
+
+            return done()
+          })
+        })
+      })
+    })
+  })
+}
+
+
 function supports_search(args = {}) {
   const { seneca } = args
   Assert(null != seneca, 'The "seneca" argument is required')
@@ -61,5 +200,8 @@ function supports_remove(args = {}) {
 module.exports = {
   supports_add,
   supports_search,
-  supports_remove
+  supports_remove,
+  add,
+  remove,
+  search
 }
